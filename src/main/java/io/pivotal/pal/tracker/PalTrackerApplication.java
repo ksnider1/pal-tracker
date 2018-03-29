5f8d9816f506f13affff9f1dfc176a96bced11bb
accenturelabs <accenturelabs@accenturelabss-iMac-3.local>
2018-03-29 17:14:03 -0400

    change return type for bean repo

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.mysql.cj.jdbc.MysqlDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,7 +16,7 @@ import javax.sql.DataSource;
 public class PalTrackerApplication {
 
     @Bean
-    public JdbcTimeEntryRepository getTimeEntryRepository(DataSource dataSource){
+    TimeEntryRepository timeEntryRepository(DataSource dataSource) {
         return new JdbcTimeEntryRepository(dataSource);
     }
 
